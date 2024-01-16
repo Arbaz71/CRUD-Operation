f70730efae3c00a2f736b4d08147e6c4e691f1dd
Ahsan Saleem Butt <57230683+imeahsan@users.noreply.github.com>
2024-01-16 18:39:42 +0500

    Add new code

--- src/app/components/user/login/login.component.ts
+++ src/app/components/user/login/login.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Init } from 'node:v8';
+import { UserService } from '../../../services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { subscribe } from 'node:diagnostics_channel';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.sass'
+  styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
+  loginForm:FormGroup
+constructor(private api:UserService, private toastr:ToastrService, private router:Router){
+   this.loginForm=new FormGroup({
+    email:new FormControl('',[Validators.required,Validators.email]),
+    password:new FormControl('',[Validators.required,Validators.minLength(8)])
+   })
+}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+onSubmit(){
+  if(this.loginForm.valid){
+    this.api.loginForm().subscribe((res: any)=>{
+      const user=res.find((loginUser:any)=>{
+        return loginUser.email==this.loginForm.value.email && loginUser.password==this.loginForm.value.password;
+      })
+      if(user){
+        localStorage.setItem("User",JSON.stringify(user));
+        this.toastr.success("Successfully Logged In");
+        this.loginForm.reset();
+        this.router.navigateByUrl('/todo')
+      }else
+      {
+        this.toastr.warning('Invalid Credentials')
+      }
 
+    },(err: any)=>{
+      alert(err);
+    }
+     )
+  }
+}
 }
