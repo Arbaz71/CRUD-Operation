3531ce8c297e30055bea879c5579698294d58640
Ahsan Saleem Butt <57230683+imeahsan@users.noreply.github.com>
2024-01-18 19:13:59 +0500

    Add new code

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  SignUpUser(value: any) {
+    throw new Error('Method not implemented.');
+  }
   loginForm() {
     throw new Error('Method not implemented.');
   }
